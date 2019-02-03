26e48b15525354a7fc7a2c4a128585e3883628cc
Marc LeBlanc <marc@stratus8.ca>
2019-02-03 20:53:31 +0000

    Change setup script to kick off ansible-playbook

--- prep-awx-centos7-setup.sh
+++ prep-awx-centos7-setup.sh
@@ -5,3 +5,5 @@ cd /root/prep-awx
 git init
 git remote add origin https://github.com/marc-leblanc/awx-ansible-install.git
 git pull https://github.com/marc-leblanc/awx-ansible-install/
+cd prep-awx
+ansible-playbook prep-awx.yml
