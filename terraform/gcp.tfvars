ebb67792e05723d3bc21a9c48d263193a57dc936
Marc LeBlanc <marc@stratus8.ca>
2019-02-16 15:49:11 -0500

    Terraform setup

--- terraform/gcp.tfvars
+++ terraform/gcp.tfvars
@@ -1,5 +1,10 @@
-gcp_json+        = "../secrets/gcp.json"
-gcp_project_id   = "myproj_id"
+# Account Setup
+gcp_json+        = "../secrets/account.json"
+gcp_project_id   = "lhq-miq-gcp"
 gcp_region       = "northamerica-northeast1"
 gcp_zone         = "a"
+
+# Instance Set up
+gcp_instance_name = "awx01.leblanchq.com"
+gcp_instance_os = "centos-cloud/centos-7"
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,43 @@ provider "google" {
   credentials = "${file("account.json")}"
   project     = "${var.gcp_project_id}"
   region      = "${var.gcp_region}"
-  zone        = "${var.gcp_zone}"
+  zone        = "${var.gcp_region}-${var.gcp_zone}"
+}
+
+// Create a new instance
+resource "google_compute_instance" "Arc1" {
+  count = 1
+  #name         = "am1-test"
+  machine_type = "n1-standard-1"
+  zone         = "${var.gcp_region}-${var.gcp_zone}"
+  name = "${var.gcp_instance_name}"
+
+
+  boot_disk {
+    initialize_params {
+      image = ${var.gcp_instance_os}
+    }
+  }
+
+ network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+   }
+}
+  ##metadata {
+    #foo = "bar"
+  #}
+
+#  metadata_startup_script = "echo hi > /test.txt"
+
+  #metadata_startup_script = "echo ${data.google_compute_address.my_address.address} > /root/yourIP"
+
+
+  #service_account {
+  #  scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  #}
+
+
 }
