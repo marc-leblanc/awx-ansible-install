1022be104168d7001538f8cd79fcb9a9617d4989
Marc LeBlanc <marc@stratus8.ca>
2019-03-09 15:57:51 -0500

    Restructuring

--- prep-awx-centos7-setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-yum -y install git ansible
-mkdir /root/prep-awx
-cd /root/prep-awx
-git init
-git remote add origin https://github.com/marc-leblanc/awx-ansible-install.git
-git pull https://github.com/marc-leblanc/awx-ansible-install/
-cd prep-awx
-ansible-playbook prep-awx.yml
