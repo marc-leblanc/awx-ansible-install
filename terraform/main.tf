c1663a4f8e6ba9a7fac465914228fab786b35ce3
marc-leblanc <2452835+marc-leblanc@users.noreply.github.com>
2019-09-11 00:36:31 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,79 @@
-module "gcp" {
-   source                 = "./modules/gcp"
-   gcp_json               = "${var.gcp_json}"
-   gcp_project_id         = "phil-test-252017"
-   gcp_region             = "northamerica-northeast1"
-   gcp_zone               = "a"
-   gcp_instance_name      = "portal01"
-   gcp_instance_os        = "centos-cloud/centos-7"
-   ssh_key_path           = "~/.ssh/"
-   ssh_key_pub            = "${var.ssh_key_pub}"
-   ssh_key_priv           = "${var.ssh_key_priv}"
-   ssh_user               = "mleblanc"
-   ansible_playbook       = "gwo-auto.yml"
-   github_repo            = "https://github.com/marc-leblanc/awx-ansible-install.git"
+provider "google" {
+  credentials = "${var.gcp_json}"
+  project     = "${var.gcp_project_id}"
+  region      = "${var.gcp_region}"
+  zone        = "${var.gcp_region}-${var.gcp_zone}"
 }
 
-# source             = module path
-# gcp_jason          = GCP Service Account Key 
-# gcp_project_id     = GCP Project ID
-# gcp_region         = GCP Region for instances ie northamerica-northeast
-# gcp_zone           = GCP Zone within the region ie a,b,c
-# gcp_instance_name  = The instance name as it will appear in GCP https://cloud.google.com/compute/docs/regions-zones/
-# gcp_instance_os    = The OS Image to use - public images https://cloud.google.com/compute/docs/images#os-compute-support
-# ssh_key_path       = Path to SSH key pair to use
-# ssh_key_pub        = Public Key filename to be provisioned to the instance
-# ssh_key_priv       = Private Key filename 
-# ssh_user           = Username for ssh
+// Create a new instance
+resource "google_compute_instance" "awx01" {
+  count        = 1
+  machine_type = "n1-standard-1"
+  zone         = "${var.gcp_region}-${var.gcp_zone}"
+  name         = "${var.gcp_instance_name}"
+
+
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_instance_os}"
+    }
+  }
+
+ network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+   }
+}
+  // Add http/https network tags for use in firewall rule creation
+  tags=["http","https"]
+  
+  // Add the public key defined in main.tf as a meta tag 
+  metadata {
+     ssh-keys = "${var.ssh_user}:${file("${var.ssh_key_path}${var.ssh_key_pub}")}"
+  }
+
+  // Remote Exec provisioner is used to bootstrap the instance with GIT/Ansible and run 
+  // the playbook to install AWX. It connects via SSH using the SSH Keys provisioned previously 
+  // as defined in main.tf 
+
+  provisioner "remote-exec" {
+    connection { 
+      type    = "ssh"
+      user    = "${var.ssh_user}"
+      timeout = "500s"
+      private_key = "${file("${var.ssh_key_path}${var.ssh_key_priv}")}"
+    }
+    inline = [
+
+      "sudo yum -y install git ansible",
+      "sudo mkdir /root/prep-awx",
+      "sudo cd /root/prep-awx",
+      "sudo git init",
+      "sudo git remote add origin https://github.com/marc-leblanc/awx-ansible-install.git",
+      "sudo git pull ${var.github_repo}",
+      "sudo cd prep-awx",
+      "sudo ansible-playbook ${var.ansible_playbook}"
+    ]
+
+  }
+
+}
+// Create a firewall Rule to allow instances tagged with http/https 
+resource "google_compute_firewall" "default" {
+ name    = "web-firewall"
+ network = "default"
+
+ allow {
+   protocol = "icmp"
+ }
+
+ allow {
+   protocol = "tcp"
+   ports    = ["80","443"]
+ }
+
+ source_ranges = ["0.0.0.0/0"]
+ target_tags = ["http","https"]
+}
